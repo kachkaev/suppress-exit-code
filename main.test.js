9cbf8db470c725fedf52d36b6094f9ec7672f1b7
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-19 00:27:07 +0100

    Switch to ESModules

--- main.test.js
+++ main.test.js
@@ -1,14 +1,14 @@
-const execa = require("execa");
+import { execa } from "execa";
 
 describe("suppress-exit-code", () => {
   it("Does not work without arguments", async () => {
-    const result = await execa("./index.js", [], { reject: false });
+    const result = await execa("./main.js", [], { reject: false });
     expect(result.stderr).toEqual("Please specify a child command to run");
     expect(result.exitCode).toEqual(1);
   });
 
   it("Forwards args and pipes stdout", async () => {
-    const result = await execa("./index.js", ["echo", "hello", "world"], {
+    const result = await execa("./main.js", ["echo", "hello", "world"], {
       reject: false,
     });
     expect(result.stdout).toEqual("hello world");
@@ -18,7 +18,7 @@ describe("suppress-exit-code", () => {
 
   it("Pipes stderr", async () => {
     const result = await execa(
-      "./index.js",
+      "./main.js",
       ["sh", "-c", "echo some stderr >&2"],
       { reject: false },
     );
@@ -28,7 +28,7 @@ describe("suppress-exit-code", () => {
   });
 
   it("Suppresses exit code", async () => {
-    const result = await execa("./index.js", ["exit", "42"], { reject: false });
+    const result = await execa("./main.js", ["exit", "42"], { reject: false });
     expect(result.stdout).toEqual("");
     expect(result.stderr).toEqual("");
     expect(result.exitCode).toEqual(0);
